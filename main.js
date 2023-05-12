065920c26306662f4a505c8798b0130233cd44cf
A12345678910raghuvanshi12345678910 <97298557+A12345678910raghuvanshi12345678910@users.noreply.github.com>
2023-05-12 13:45:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video = "";
+status_js = "";
+
+function preload() {
+    video = createVideo("video.mp4");
+    video.hide();
+}
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+function draw() {
+    image(video,0,0,480,380);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting objectes";
+}
+function modelLoaded() {
+    console.log("model loaded");
+    status_js = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
